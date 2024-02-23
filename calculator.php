eb671c66a5e0270c86e6f52708e5d3b0abb6a7f0
Felix Huang <felixhuang12@gmail.com>
2024-02-23 12:03:58 -0500

    Add simple calculator

---
+++ calculator.php
@@ -0,0 +1,52 @@
+<center>
+    <?php
+        echo "<form method='post' action=''>";
+        echo "<input type='text' name='num1' placeholder='First number'>";
+        echo "<input type='text' name='num2' placeholder='Second number'>";
+        echo "<select name='operator' id='operator'>";
+        echo "<option value='add'>Add</option>";
+        echo "<option value='subtract'>Subtract</option>";
+        echo "<option value='multiply'>Multiply</option>";
+        echo "<option value='divide'>Divide</option>";
+        echo "</select>";
+        echo "<input type='submit' name='submit'>";
+        echo "</form>";
+
+        function getResult($a, $b, $operator) {
+            switch ($operator) {
+                case 'add':
+                    return $a + $b;
+                    break;
+
+                case 'subtract':
+                    return $a - $b;
+                    break;
+
+                case 'multiply':
+                    return $a * $b;
+                    break;
+
+                case 'divide':
+                    if ($b != 0) {
+                        return $a / $b;
+                    } else {
+                        return "Division by zero error";
+                    }
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+
+        if (isset($_POST['submit'])) {
+            $result = getResult($_POST['num1'], $_POST['num2'], $_POST['operator']);
+
+            if ($result !== FALSE) {
+                echo "<br>" . $result;
+            } else {
+                echo "An error occurred";
+            }
+        }
+    ?>
+</center>
