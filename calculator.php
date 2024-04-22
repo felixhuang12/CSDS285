61d68dcb4260f7d154bf89b5f41be4f8db1fa855
Felix Huang <felixhuang12@gmail.com>
2024-04-22 15:05:22 -0400

    Remove redundnant break statements

--- calculator.php
+++ calculator.php
@@ -16,15 +16,12 @@
             switch ($operator) {
                 case 'add':
                     return $a + $b;
-                    break;
 
                 case 'subtract':
                     return $a - $b;
-                    break;
 
                 case 'multiply':
                     return $a * $b;
-                    break;
 
                 case 'divide':
                     if ($b != 0) {
@@ -32,7 +29,6 @@
                     } else {
                         return "Division by zero error";
                     }
-                    break;
 
                 default:
                     return false;
